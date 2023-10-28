0c92dece112fe0483441fbddcf86b4efa8f67b9a
Olga <helga_ser@mail.ru>
2023-10-28 22:56:21 +0300

    Initial

---
+++ src/test/java/ru/netology/javaqa/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldSendReturnData() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Добрый вечер")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Добрый вечер"));
+    }
+
+
+}
